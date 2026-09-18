6bc1069234d35fbdead6f8950ecad45296e57920
Sam Mooney <sammooneydev@gmail.com>
2026-09-18 11:33:57 +0100

    added flash data functionality to admin panel, as well as gave form section the profile class, to style it in the same way as the profile page

--- app/Views/pages/admin-panel.php
+++ app/Views/pages/admin-panel.php
@@ -6,27 +6,42 @@
     <?= view('templates/header')?>
         <body>
             <div class="main-content">
-                <form class="login" method="post" action="<?= base_url('admin/create-film') ?>">
-                <h2>Add Film to Database</h2>
-                <?= csrf_field() ?>
 
-                <label for="film_name">Film Name</label>
-                <input type="text" name="film_name" id="film_name" required>
+                <div class="profile">
+                        <?php if(session()->getFlashdata('success')):?>
+                            <p class="success">
+                                <?= esc(session()->getFlashdata('success'))?>
+                            </p>
+                        <?php endif;?>
 
-                <br><br>
+                        <?php if(session()->getFlashdata('error')):?>
+                            <p class="error">
+                                <?= esc(session()->getFlashdata('error'))?>
+                            </p>
+                        <?php endif;?>
 
-                <label for="film_desc">Description</label>
-                <textarea name="film_desc" id="film_desc"></textarea>
+                        <form class="login" method="post" action="<?= base_url('admin/create-film') ?>">
+                        <h2>Add Film to Database</h2>
+                        <?= csrf_field() ?>
 
-                <br><br>
+                        <label for="film_name">Film Name</label>
+                        <input type="text" name="film_name" id="film_name" required>
 
-                <label for="director_name">Director</label>
-                <input type="text" name="director_name" id="director_name" required>
+                        <br><br>
 
-                <br><br>
+                        <label for="film_desc">Description</label>
+                        <textarea name="film_desc" id="film_desc"></textarea>
 
-                <button type="submit">Add Film</button>
-            </form>
+                        <br><br>
+
+                        <label for="director_name">Director</label>
+                        <input type="text" name="director_name" id="director_name" required>
+
+                        <br><br>
+
+                        <button type="submit">Add Film</button>
+                    </form>
+                </div>
             </div>
         </body>
     <?= view('templates/footer')?>
